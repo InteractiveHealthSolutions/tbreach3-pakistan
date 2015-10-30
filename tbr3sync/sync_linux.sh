897b2796f0830f28c210f38893cdb46dd431b3c5
Tahira <Tahira@199.172.1.44>
2015-10-30 16:09:40 +0500

    - Updated sync script files

--- tbr3sync/sync_linux.sh
+++ tbr3sync/sync_linux.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Script for synchronizing separate openmrs instances to central openmrs
-sudo rm -R /tmp/sz
-sudo mkdir /tmp/sz
+rm -R /tmp/sz/*
+service tomcat6 stop
 mysql -u root -p openmrs -e "call openmrs.generate_offline_data_csv(4)"
-mysql -h 202.141.249.106 -P 8933 --local-infile -u root -p openmrs < /home/openmrs/Documents/TBR3\ Stored\ Procedures/load_data_into_temp_tables.sql
-mysql -h 202.141.249.106 -P 8933 -u root -p openmrs -e "call openmrs.sync_openmrs(4)"
+mysql -h 202.141.249.106 -P 8940 --local-infile -u root -p openmrs < /home/openmrs/Documents/TBR3\ Stored\ Procedures/load_data_into_temp_tables.sql
+mysql -h 202.141.249.106 -P 8940 -u root -p openmrs -e "call openmrs.sync_openmrs(4)"
+service tomcat6 start
